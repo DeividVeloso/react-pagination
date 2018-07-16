30a2fdcaec80d54306d212ea01d314af81b00109
Deivid Veloso <veloso.deivid@gmail.com>
2018-07-16 18:26:32 -0300

    add pagination

---
+++ src/components/helpers.js
@@ -0,0 +1,18 @@
+export const LEFT_PAGE = 'LEFT';
+export const RIGHT_PAGE = 'RIGHT';
+
+/**
+ * Helper method for creating a range of numbers
+ * range(1, 5) => [1, 2, 3, 4, 5]
+ */
+export const range = (from, to, step = 1) => {
+  let i = from;
+  const range = [];
+
+  while (i <= to) {
+    range.push(i);
+    i += step;
+  }
+
+  return range;
+}
